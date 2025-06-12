c57e4a181c3712362ca279f51a4eb5791d71370d
Dhalish <dhalishraj123@gmail.com>
2025-06-12 09:51:56 +0530

    initial commit

---
+++ a.py
@@ -0,0 +1,25 @@
+#a=10
+#b=20
+
+#print("a=",b)
+#print("b=",a)
+
+#b=a+b
+#a=b-a
+#b=b-a
+#print (a)
+#print(b)
+
+
+#a,b=b,a
+#print(a)
+#print(b)
+
+
+# print("a=",a,"b=",b)
+# print("a={} b{}".format(a,b))
+# print(f"a={a} b{b}")
+
+# a="hgf"
+# a=10
+# print(type(a))
